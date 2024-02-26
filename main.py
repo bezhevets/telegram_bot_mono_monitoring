1c0c80e3ede0f6225d6c718aa22d0ef7d979e937
Maksym Bezhevets <begik4@gmail.com>
2024-02-26 17:19:01 +0200

    Created echo bot

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+
+import telebot
+from dotenv import load_dotenv
+
+load_dotenv()
+
+bot = telebot.TeleBot(os.environ["TG_TOKEN"])
+
+
+@bot.message_handler(commands=["start", "help"])
+def send_welcome(message):
+    bot.reply_to(message, "Howdy, how are you doing?")
+
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+
+bot.infinity_polling()
